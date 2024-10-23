0fbea1a5a11c8eaeeda1f8683437c8be218eca20
Joseph Holten <joseph@holten.com>
2024-10-23 16:57:05 -0400

    basic detection pipeline

--- mldraw.cpp
+++ mldraw.cpp
@@ -3,9 +3,9 @@
 #include <fmt/ranges.h>
 #include <vector>
 
-void drawInterface() {
-
-}
+#ifndef Real
+#define Real float
+#endif
 
 int main() {
     const int initScreenWidth = 400;
@@ -25,7 +25,11 @@ int main() {
     bool takingImage = false;
 
     char detected = '_';
-    char text[] = "detected: _";
+    float confidence = 0.0;
+    char text[] = "detected: _ (0.00)";
+
+    // TODO
+    // Model model = jmyml::nn::load("models/mnist-good.model");
 
     while (!WindowShouldClose()) {
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
@@ -50,7 +54,7 @@ int main() {
         BeginDrawing(); {
             if (!takingImage) {
                 DrawText("please draw a digit, enter to detect, 'c' to clear, 'ESC' to quit", 0, 0, 22, GRAY);
-                sprintf(text, "detected: %c", detected);
+                sprintf(text, "detected: %c (%.2f)", detected, confidence);
                 DrawText(text, 0, 25, 22, GRAY);
             }
             for (auto line : lines) {
@@ -63,13 +67,22 @@ int main() {
                 if (image.format != PIXELFORMAT_UNCOMPRESSED_R8G8B8A8) {
                     TraceLog(LOG_ERROR, "image taken from screen is in unrecognized format");
                 } else {
+                    // 1. step: convert to PIXEL_FORMAT_UNCOMPRESSED_GRAYSCALE
+                    ImageFormat(&image, PIXELFORMAT_UNCOMPRESSED_GRAYSCALE);
+
+                    // 2. step: crop to content (square)
+                    // TODO
+
+                    // 3. step: scale to 28x28
+                    // TODO
+
+                    // 4. step: send it through NN
+                    // std::vector<Real> preds = Model.forward(image.data, image.width*image.height);
+                    // auto max_it = std::max_element(preds.begin(), preds.end());
+                    // detected = max_it - preds.begin() + '0';
+                    // confidence = *max_it;
+                    detected = '?';
                     TraceLog(LOG_ERROR, "detection unimplemented");
-                    // TODO:
-                    //   - convert to PIXEL_FORMAT_UNCOMPRESSED_GRAYSCALE
-                    //   - crop to content (square)
-                    //   - scale it to 28x28
-                    //   - send it through NN
-                    //   - display prediction from NN
                 }
                 takingImage = false;
             }
