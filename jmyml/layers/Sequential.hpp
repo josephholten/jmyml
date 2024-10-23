2646261823920fc99292184e60aa8c37debe25d4
Joseph Holten <joseph@holten.com>
2024-10-23 14:10:43 -0400

    correct sequential container

--- jmyml/layers/Sequential.hpp
+++ jmyml/layers/Sequential.hpp
@@ -1,26 +1,53 @@
-// TODO: variadic template parameters: this allows us to have Layer not be a virtual class, decreasing overhead and incresing speed (?)
+#include <tuple>
 #include <cstddef>
-#include <CL/sycl.hpp>
-#include <jmyml/layers/Layer.hpp>
+#include <sycl/sycl.hpp>
+#include <fmt/ranges.h>
 
-using namespace cl;
+#ifndef Real
+#define Real float
+#endif
 
-template<class... Layers, typename Real = DefaultReal>
+namespace jmyml {
+
+template<class... Layers>
 class Sequential {
+public:
+    using Tuple = std::tuple<Layers...>;
+
+    static constexpr size_t in_dim  = std::tuple_element<0,Tuple>::type::in_dim;
+    static constexpr size_t out_dim = std::tuple_element<sizeof...(Layers)-1,Tuple>::type::out_dim;
+
+    Sequential(Layers... _layers)
+        : layers{_layers...}
+    { }
+
+    template<int I = 0>
     void forward(sycl::queue& Q, sycl::buffer<Real>& x, sycl::buffer<Real>& y) {
-        sycl::buffer<Real> in_buf = x;
-        std::apply([](auto&&... layer){
-            (({
-                sycl::buffer<Real> out_buf{sycl::range{layer.out_dim}};
-                layer.forward(in_buf, out_buf);
-                in_buf = out_buf;
-            }), ...);
-        }, layers);
+        static_assert(I < sizeof...(Layers));
+
+        using LayerI = std::tuple_element<I,Tuple>::type;
+        LayerI layer = std::get<I>(layers);
+        assert(x.size() == LayerI::in_dim);
+
+        sycl::buffer<Real> out{sycl::range{LayerI::out_dim}};
+        layer.forward(Q, x, out);
+
+        // if not last
+        if constexpr (I != sizeof...(Layers)-1) {
+            using LayerNext = std::tuple_element<I+1,Tuple>::type;
+            static_assert(LayerI::out_dim == LayerNext::in_dim);
+            forward<I+1>(Q, out, y);
+        }
+        // else done
+        else {
+            y = out; // by reference
+        }
     };
+
     // void backward();
 
 private:
-    size_t in_dim;
-    size_t out_dim;
     std::tuple<Layers...> layers;
 };
+
+}
---
+++ jmyml/layers/Sequential.test.cpp
@@ -0,0 +1,27 @@
+#include <jmyml/layers/Sequential.hpp>
+#include <jmyml/layers/LinearLayer.hpp>
+#include <jmyml/layers/ActivationLayer.hpp>
+#include <gtest/gtest.h>
+
+TEST(Sequential, basic) {
+    using namespace jmyml;
+    Sequential s = {
+        LinearLayer<4,4>::make_constant(10,4),  // exp. 404.0, 404.0, 404.0, 404.0
+        // ReLuLayer<3>(),
+        LinearLayer<4,2>::make_constant(.5,.1), // exp.
+    };
+
+    std::array<Real,4> xin = {10, 10, 10, 10};
+    std::array<Real,2> yout = {0, 0};
+    std::array<Real,2> yexp = {808.1, 808.1};
+    {
+        sycl::buffer<Real> x{xin};
+        sycl::buffer<Real> y{yout};
+        sycl::queue Q;
+        s.forward(Q, x, y);
+        sycl::host_accessor py{y};
+        std::copy(py.begin(), py.end(), yout.begin());
+    } // buffer destruction implies write-back to host
+
+    EXPECT_EQ(yout, yexp);
+}
