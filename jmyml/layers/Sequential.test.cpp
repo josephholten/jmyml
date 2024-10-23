53e0d5000f7ffda1c0900cbc42cf77b4242181c5
Joseph Holten <joseph@holten.com>
2024-10-23 14:24:41 -0400

    better sequential test

--- jmyml/layers/Sequential.test.cpp
+++ jmyml/layers/Sequential.test.cpp
@@ -7,13 +7,35 @@ TEST(Sequential, basic) {
     using namespace jmyml;
     Sequential s = {
         LinearLayer<4,4>::make_constant(10,4),  // exp. 404.0, 404.0, 404.0, 404.0
-        // ReLuLayer<3>(),
         LinearLayer<4,2>::make_constant(.5,.1), // exp.
     };
 
     std::array<Real,4> xin = {10, 10, 10, 10};
     std::array<Real,2> yout = {0, 0};
     std::array<Real,2> yexp = {808.1, 808.1};
+    {
+        sycl::buffer<Real> x{xin};
+        sycl::buffer<Real> y{yout};
+        sycl::queue Q;
+        s.forward(Q, x, y);
+        sycl::host_accessor py{y};
+        std::copy(py.begin(), py.end(), yout.begin());
+    } // buffer destruction implies write-back to host
+
+    EXPECT_EQ(yout, yexp);
+}
+
+TEST(Sequential, withReLu) {
+    using namespace jmyml;
+    Sequential s = {
+        LinearLayer<4,4>::make_constant(-10,4),  // exp. 404.0, 404.0, 404.0, 404.0
+        ReLuLayer<4>(),
+        LinearLayer<4,2>::make_constant(.5,.1), // exp.
+    };
+
+    std::array<Real,4> xin = {10, 10, 10, 10};
+    std::array<Real,2> yout = {0, 0};
+    std::array<Real,2> yexp = {.1,.1};
     {
         sycl::buffer<Real> x{xin};
         sycl::buffer<Real> y{yout};
